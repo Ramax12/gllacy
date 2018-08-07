1f5f6900db4ad3257c59dac3fd6cceea3131c21e
Maksim Ko <Ramax13@gmail.com>
2018-08-07 15:14:20 +0300

    Оптимизация, добавление js на user-block, минимизация

--- js/script.js
+++ js/script.js
@@ -1,37 +1,102 @@
 'use strict';
 
 (function () {
-var btn = document.querySelector(".form_btn");
-var modal = document.querySelector(".modal");
-var close = document.querySelector(".exit");
+  var ESC_KEYCODE = 27;
 
-//open modal_content
-btn.addEventListener("click", function (evt) {
-  event.preventDefault();
-  modal.classList.add("modal_show");
-});
+  var containerMainHeader = document.querySelector('.main-header');
+  var containerNavToggle = document.querySelector('.main-nav__toggle');
 
-//close modal_content(click on button)
-close.addEventListener("click", function (evt) {
-  event.preventDefault();
-  modal.classList.remove("modal_show");
-});
+  //burger menu
+  containerMainHeader.classList.add('main-header--closed');
+  containerNavToggle.addEventListener('click', function() {
+    if (containerMainHeader.classList.contains('main-header--closed')) {
+      containerMainHeader.classList.remove('main-header--closed');
+      containerMainHeader.classList.add('main-header--opened');
+    } else {
+      containerMainHeader.classList.add('main-header--closed');
+      containerMainHeader.classList.remove('main-header--opened');
+    }
+  });
 
-//close modal_content(click on modal)
-window.addEventListener("click", function (evt) {
-  if (event.target == modal) {
-    if (modal.classList.contains("modal_show")){
-      modal.classList.remove("modal_show");
+  var containersUserBlockItem = document.querySelectorAll('.user-block_item');
+  var containersWrapperBlock = document.querySelectorAll('.wrapper_block');
+
+  //close wrapper_block
+  function closePopup () {
+    [].forEach.call(containersWrapperBlock, function (item) {
+      if (item.style.display == 'block') {
+        item.style.display = 'none';
+      }
+    });
+  }
+
+  function top_walker (node, test_func) {
+    while ( node) {
+      if ( test_func(node) ) {
+        return node;
+      }
+      node = node.parentNode;
     }
   }
-});
 
-//close modal_content(push on ESC)
-window.addEventListener("keydown", function (evt) {
-  if (event.keyCode === 27) {
-    if (modal.classList.contains("modal_show")){
-      modal.classList.remove("modal_show");
+  document.addEventListener('click', function (event) {
+    var openedPopup = top_walker(event.target, function (node) {
+      return node === containersWrapperBlock;
+    });
+    if (!openedPopup) {
+      closePopup(containersWrapperBlock);
     }
-  }
-});
+  }, true);
+
+  //open wrapper_block
+  [].forEach.call(containersUserBlockItem, function (parent) {
+    parent.addEventListener('click', function(){
+      var daughter = parent.querySelectorAll('.wrapper_block');
+      [].forEach.call(daughter, function (item) {
+        item.style.display = 'block';
+        document.addEventListener('keydown', onPopupEscPress);
+      });
+    });
+  });
+
+  //close wrapper_block(push on ESC)
+  var onPopupEscPress = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closePopup(containersWrapperBlock);
+    }
+  };
+
+  var containerFormBtn = document.querySelector('.form_btn');
+  var containerModal = document.querySelector('.modal');
+  var containerExit = document.querySelector('.exit');
+
+  //open modal_content
+  containerFormBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    containerModal.classList.add('modal_show');
+  });
+
+  //close modal_content(click on button)
+  containerExit.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    containerModal.classList.remove('modal_show');
+  });
+
+  //close modal_content(click on modal)
+  window.addEventListener('click', function (evt) {
+    if (evt.target == containerModal) {
+      if (containerModal.classList.contains('modal_show')){
+        containerModal.classList.remove('modal_show');
+      }
+    }
+  });
+
+  //close modal_content(push on ESC)
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      if (containerModal.classList.contains('modal_show')){
+        containerModal.classList.remove('modal_show');
+      }
+    }
+  });
 })();
